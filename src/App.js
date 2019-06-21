99097fcbfd4b8cb2adb864480dc3e3ed0add0e08
Dystropolis <46174788+Dystropolis@users.noreply.github.com>
2019-06-20 21:15:45 -0700

    Made basic Navigation Bar component. (#4)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { AppBody } from './components/AppBody/Index';
+import { NavBar } from './components/NavBar';
 
 function App() {
   return (
     <div>
       <Header/>
+      <NavBar/>
       <AppBody/>
     </div>
   );
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import  './NavBar.css';
+
+export class NavBar extends React.Component<props>{
+    render() {
+        return (
+            <div className="NavBar">
+                
+            </div>
+        );
+    }
+}
