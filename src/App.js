6da9bdc2c330f398668ac31e52f94cbb36d4758d
unknown <dystro101@gmail.com>
2019-06-20 20:49:39 -0700

    Created app body cmp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Header } from './components/Header';
+import { AppBody } from './components/AppBody/Index';
 
 function App() {
   return (
+    <div>
    <Header/>
+   <AppBody/>
+   </div>
   );
 }
 
---
+++ src/components/AppBody/Index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  './AppBody.css';
+
+
+export class AppBody extends React.Component<props>{
+    render() {
+        return (
+            <div className="AppBody">
+                
+            </div>
+        );
+    }
+}
